dd0879bf0a45194cd22bc27522449ff0043d99bd
Ayrton <a@a.com>
2024-09-06 00:51:25 -0600

    Added cloudfared

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,7 +12,7 @@
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
+    # outputs.homeManagerModules.emacs
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
@@ -66,6 +66,8 @@
     thunderbird  # email
     emacs  # the real os
     localsend  # airdrop
+    openssh
+    cloudflared
   ];
 
   home.sessionVariables = {
